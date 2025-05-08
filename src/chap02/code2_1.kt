67eeca97ca29d49b230ee4c7af19ed440371df7a
itc-s24006 <s24006@std.it-college.ac.jp>
2025-05-08 12:41:42 +0900

    first commit

---
+++ src/chap02/code2_1.kt
@@ -0,0 +1,14 @@
+package chap02
+
+/**
+ * ／＊＊で始めるとdocコメントと呼ばれ、スクリプトとして呼び出すときに補完として説明されたり、
+ * カーソルを合わせたときに説明書きとして表示される
+ * We declare a package-level function main which returns Unit and takes
+ * an Array of strings as a parameter. Note that semicolons are optional.
+ */
+
+//これはコメントです。
+//何でも好きなことを書いてください。
+fun main(args: Array<String>) {
+    println("Hello, world!")
+}
---
+++ src/chap02/code2_4.kt
@@ -0,0 +1,8 @@
+package chap02
+
+//ctrl+dでカーソル行を複製
+fun main() {
+    println("こんにちは")
+    println("はじめまして")
+    println("よろしく！")
+}
